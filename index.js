0c33b471987b271d439a5aabe3a87c3f19d2f6a6
Maxwell <lubalecliff@gmail.com>
2023-10-05 12:51:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+
+let number = isInteger
+let isInteger = random
+let random = 1
+
+let num3 = 9
+let num4 = 18
+let mod = 4
+
+let max = 20
+
